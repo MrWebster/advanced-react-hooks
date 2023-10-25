87fac0d476db3d23651987efc21181bf7fd1ef23
Andrew Webster <mr.andrewwebster@gmail.com>
2023-10-25 16:22:57 -0400

    exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,16 +4,53 @@
 import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  function countReducer(state, newState) {
+    return newState
+  }
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
-  return <button onClick={increment}>{count}</button>
+  function countReducerExtraCredit1(state, newState) {
+    return state + newState
+  }
+
+  function countReducerExtraCredit2(state, newState) {
+    return {...state, ...newState}
+  }
+
+  function countReducerExtraCredit3(state, newState) {
+    return typeof newState === 'function'
+      ? newState(state)
+      : {...state, ...newState}
+  }
+
+  function countReducerExtraCredit4(state, action) {
+    console.log('into reducer')
+    if (typeof action === 'function') {
+      return action(state)
+    }
+
+    console.log('past function check')
+
+    switch (action.type) {
+      case 'increment':
+        console.log('into switch of type increment')
+        return {...state, count: state.count + action.step}
+      default:
+        throw new Error(`unhandled action type: ${action.type}`)
+    }
+  }
+
+  const [state, dispatch] = React.useReducer(countReducerExtraCredit4, {
+    count: initialCount,
+  })
+
+  const {count} = state
+
+  const increment1 = {type: 'increment', step}
+
+  // const increment = () => dispatch({count: count + step});
+  // const incrementExtra3 = () => dispatch(state => ({count: state.count + step}));
+  const incrementExtra4 = () => dispatch(increment1)
+  return <button onClick={incrementExtra4}>{count}</button>
 }
 
 function App() {
